1f203422f83e77dc79d3c1cd477f2d6852c27bea
Vasaant Krishnan <vasaantk@gmail.com>
2020-01-30 20:49:31 +0200

    Basic shape complete

--- auto/Lecture.el
+++ auto/Lecture.el
@@ -3,16 +3,16 @@
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("article" "11pt" "a4paper")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
-    "tex/01-Intro"
+    "tex/01-Figure"
     "article"
     "art11"
     "amsmath"
@@ -21,6 +21,7 @@
     "a4wide"
     "cancel"
     "subcaption"
+    "tikz"
     "appendix")
    (TeX-add-symbols
     "msp")
